5ff11948abb5b7b7dd0182a5795dfacfec7ca17b
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-01-20 20:47:14 +0100

    devnet smart contract deployment completed

--- src/solana/constants.js
+++ src/solana/constants.js
@@ -1,8 +1,8 @@
 export const PRESALE_TOKEN_DECIMALS = 5;
 export const PRESALE_PROGRAM_ID = "HvMPaosbn84f8cABpWvmma5R1XUneyQY6YSpHzZrWE5z";
-export const PRESALE_TOKEN_MINT = "9e4U5CwGXaAf8d1XipoWNn2V6U7h4BM8UpYNdDkPkP6G";
-export const PRESALE_OWNER_PUB_KEY = "56soKhmhDDx9A4BUqVZmr7ENqBxroFZmtrNTzw47S5Dr";
-export const PRESALE_STATE_ACCOUNT_PUB_KEY = "G7RSwCQeJVRkgPspTXu2PQLGA3134p6SyMnPj6Wuwmue";
-export const PRESALE_TOKEN_ACCOUNT_PUB_KEY = "Csjw6CJo9MKA9GYrsxVm1tEogEqTtRK6oemCdjzmVhPg";
-export const TOKEN_PRICE_PER_SOL = 90000;
+export const PRESALE_TOKEN_MINT = "CchGAGwL8tDVWFLfp99hiVeFYmfmitX3tJ4EagsgBkzc";
+export const PRESALE_OWNER_PUB_KEY = "E2F3fsS1HpsLb2VpEgsA5ztfo83CWFWW4jWpC6FvJ6qR";
+export const PRESALE_STATE_ACCOUNT_PUB_KEY = "C7Q8RcVLdgy4jYtud9txphnm84FUBf8o7RWshQenZCMz";
+export const PRESALE_TOKEN_ACCOUNT_PUB_KEY = "GWStpHW15EwCN2YF8Z4XjpBJcPDZVnCTHBP1gWHmsjZC";
+export const TOKEN_PRICE_PER_SOL = 900000;
 export const SOLANA_CONNECTION_URL = "https://api.devnet.solana.com";
