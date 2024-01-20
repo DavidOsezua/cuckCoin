a2dab26d38b1db52317ad51ba7cab3ca574fec24
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-01-20 19:52:11 +0100

    fixed frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,18 @@ import { logo, linkArrow } from "../assets";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 const Navbar = () => {
-
   return (
     <header className={`${styles.header}`}>
       <nav className={`${styles.navContainer}`}>
         <img src={logo} alt="Logo" className={styles.logo} />
         <div className="flex items-center gap-[3px]">
-          <a href="/" className="text-[#fff] text-[300]">
+          <a href="http://cucktoken.xyz" className="text-[#fff] text-[300]">
             {" "}
             Back Home{" "}
           </a>
           <img src={linkArrow} />
         </div>
-        <WalletMultiButton style={{backgroundColor: '#512da8'}}/>
+        <WalletMultiButton style={{ backgroundColor: "#512da8" }} />
       </nav>
     </header>
   );
