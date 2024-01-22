b8294e5e365632f49d37f6a95b845c88ebb4448f
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-01-22 01:13:59 +0100

    modified time logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,10 +2,18 @@ import { useState, useEffect } from 'react';
 import { twoDots } from '../assets';
 import styles from './Timer.module.css';
 
-const COUNTDOWN_TARGET = new Date('2024-01-25T23:59:59');
+const COUNTDOWN_TARGET = new Date('2024-01-19T23:59:59');
 
 const getTimeLeft = () => {
   const totalTimeLeft = COUNTDOWN_TARGET - new Date();
+  if (totalTimeLeft < 0) {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
   const days = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((totalTimeLeft / (1000 * 60)) % 60);
